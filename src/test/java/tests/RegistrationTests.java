664a217d9fbc10bb693eb13033bb2664053b0dec
Oleg Razumov <orazumoff@gmail.com>
2026-04-15 15:57:35 +0300

    add faker and given()

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -3,14 +3,20 @@ package tests;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class RegistrationTests {
 
     @Test
-    public void successfulRegistrationTest{
+    public void successfulRegistrationTest() {
 
         Faker faker = new Faker();
+        String username = faker.name().fullName();
+        String password = faker.name().firstName();
+
+        String data = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
 
         //body {
         // "username":"oleg",
@@ -18,6 +24,13 @@ public class RegistrationTests {
         // http://bookclub.qa.guru:8000/api/v1/user/register/
 
         given()
+                .body(data)
+                .when()
+                .post("http://bookclub.qa.guru:8000/api/v1/user/register/")
+                .then()
+                .statusCode(201)
+                .body("username", is(username))
+                .body("id", notNullValue());
 
     }
 
