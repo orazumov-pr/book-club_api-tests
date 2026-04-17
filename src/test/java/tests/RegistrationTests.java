d4e3c61d248bb7cf1581215edd045033f5210f69
Oleg Razumov <orazumoff@gmail.com>
2026-04-17 15:57:59 +0300

    add lombok

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,12 +1,14 @@
 package tests;
 
 import com.github.javafaker.Faker;
+import models.RegistrationBodyLombokModel;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegistrationTests {
 
@@ -64,6 +66,30 @@ public class RegistrationTests {
                 .log().all()
                 .statusCode(400)
                 .body("username[0]", is("A user with that username already exists."));
-
     }
+
+    @Test
+     public void successfulRegistrationTest_with_lombok(){
+        RegistrationBodyLombokModel data = new RegistrationBodyLombokModel();
+        data.setUsername(username);
+        data.setUsername(password);
+
+//        RegistrationBodyLombokModel data = new RegistrationBodyLombokModel(username, password);
+
+        RegistrationBodyLombokModel registrationResponse = given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("http://bookclub.qa.guru:8000/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .as(RegistrationBodyLombokModel.class);
+
+        assertEquals(username, registrationResponse.getUsername());
+    }
+
+
 }
